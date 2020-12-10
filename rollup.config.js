e3bf1522dd130837cd15644f2f3b519480c7fe2f
jgw96 <jgw9617@gmail.com>
2020-12-10 14:42:55 -0800

    perf(): build updates

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,8 @@ import { injectManifest } from 'rollup-plugin-workbox';
 import html from '@open-wc/rollup-plugin-html';
 import strip from '@rollup/plugin-strip';
 import copy from 'rollup-plugin-copy';
+import alias from "@rollup/plugin-alias";
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 
 export default {
   input: 'index.html',
@@ -14,7 +16,14 @@ export default {
   },
   plugins: [
     resolve(),
+    minifyHTML(),
     html(),
+    alias({
+      entries: [{
+        find: 'lit-html/lib/shady-render.js',
+        replacement: 'node_modules/lit-html/lit-html.js'
+      }]
+    }),
     terser(),
     strip({
       functions: ['console.log']
